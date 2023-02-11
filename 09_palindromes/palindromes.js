feff8220a4f60ded16173379427f9ca339533de0
Milton Sanchez <miltonsanchez5081@gmail.com>
2023-02-11 18:03:05 -0500

    Completed palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const altString = string.toLowerCase().replace(/[^a-z]/g, "");
+  return altString.split("").reverse().join("") == altString;
 };
 
 // Do not edit below this line
